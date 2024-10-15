4e4ed74398a93a26c6b2b61ef3a8fafe7e3a59b9
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2024-10-15 22:12:23 +0900

    Refactor IDCardFactory to track owners of created IDCards

--- src/04_FactoryMethod/IDCardFactory/IDCardFactory.ts
+++ src/04_FactoryMethod/IDCardFactory/IDCardFactory.ts
@@ -3,11 +3,19 @@ import { Product } from "./Product";
 import { IDCard } from "./IDCard";
 
 export class IDCardFactory extends Factory {
+  private owners: string[] = [];
+
   protected createProduct(owner: string): Product {
     return new IDCard(owner);
   }
 
   protected registerProduct(product: Product): void {
+    const owner = (product as IDCard).getOwner();
+    this.owners.push(owner);
     console.log(`${(product as IDCard).printCardInfo()}を登録しました。`);
+  }
+
+  getOwners(): string[] {
+    return this.owners;
   }
 }
--- src/04_FactoryMethod/IDCardFactory/index.ts
+++ src/04_FactoryMethod/IDCardFactory/index.ts
@@ -2,15 +2,18 @@ import { Factory } from "./Factory";
 import { Product } from "./Product";
 import { IDCardFactory } from "./IDCardFactory";
 
-const factory: Factory = new IDCardFactory();
+const factory = new IDCardFactory();
 
 const card1: Product = factory.create("TARO");
+console.log(factory.getOwners());
 card1.use();
 console.log("==============");
 
 const card2: Product = factory.create("JIRO");
+console.log(factory.getOwners());
 card2.use();
 console.log("==============");
 
 const card3: Product = factory.create("SABURO");
+console.log(factory.getOwners());
 card3.use();
